52cff25daad0fd5ad4abdfae5183be2ad2635767
hostilelogout <80536041+hostilelogout@users.noreply.github.com>
2023-01-20 12:13:56 +0100

    Added title
    added title to header

--- src/Views/HeaderView.jsx
+++ src/Views/HeaderView.jsx
@@ -6,7 +6,8 @@ const HeaderView = () => {
             <div className="px-3 py-2 text-bg-dark">
                 <div className="container">
                     <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                        <img className="bi me-2" width={"40px"} height={"32px"} src={HeaderLogo} alt="Header Logo"></img>
+                        <img className="bi me-2" width={"50px"} height={"50px"} src={HeaderLogo} alt="Header Logo"></img>
+                        <span className="fs-4">Lost in TranSlation</span>
                     </div>
                 </div>
             </div>
