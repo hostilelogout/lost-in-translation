36d374c290738d18b8302961ab6e4c18da392345
hostilelogout <80536041+hostilelogout@users.noreply.github.com>
2023-01-20 11:45:08 +0100

    Updated header to show logo
    added another logo
    logo now displays correctly

--- src/Views/HeaderView.jsx
+++ src/Views/HeaderView.jsx
@@ -6,9 +6,7 @@ const HeaderView = () => {
             <div className="px-3 py-2 text-bg-dark">
                 <div className="container">
                     <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                        <svg className="bi me-2" width={"40px"} height={"32px"} role="img" aria-label="Bootstrap">
-                            <img src={HeaderLogo} alt="Header Logo"></img>
-                        </svg>
+                        <img className="bi me-2" width={"40px"} height={"32px"} src={HeaderLogo} alt="Header Logo"></img>
                     </div>
                 </div>
             </div>
