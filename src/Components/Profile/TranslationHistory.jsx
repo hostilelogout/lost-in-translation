a890a2340034ec3fe84492eccf5c217eb95b272a
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-01-25 12:58:09 +0100

    Added comments to: TranslationHistory.jsx TranslationForm.jsx Async.jsx Authentication.jsx

--- src/Components/Profile/TranslationHistory.jsx
+++ src/Components/Profile/TranslationHistory.jsx
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { getAsync, updateAsync } from "../User/Async";
 
+//Component for the main profile page.
+//Display translations from API.
+//Clear translations in API.
 const TranslationHistory = () => {
     const [ translations, setTranslations] = useState([]);
     useEffect(() => {
@@ -11,6 +14,7 @@ const TranslationHistory = () => {
         getTranslations()
     }, [])
 
+    //Create list items from translations array for display.
     const translationList = () => {
         const listItems = translations.map((translation, index) =>
             <li className="list-group-item" key={index}>{translation}</li>
@@ -18,6 +22,9 @@ const TranslationHistory = () => {
           return <ul className="list-group">{listItems}</ul>;
     }
     
+    //Handle clear translations event.
+    //Clears translations in API.
+    //Clears translations in state.
     const handleSubmit = (event) => {
         event.preventDefault()
         updateAsync([], false)
